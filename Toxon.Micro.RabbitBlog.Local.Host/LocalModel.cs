18460d17e242e84e8e0260306b3bedb7117fc7b9
Will Smith <will@toxon.co.uk>
2019-04-20 15:12:54 +0100

    Create host for local debugging of all services

---
+++ Toxon.Micro.RabbitBlog.Local.Host/LocalModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Toxon.Micro.RabbitBlog.Core;
+using Toxon.Micro.RabbitBlog.Routing;
+using Toxon.Micro.RabbitBlog.Routing.Patterns;
+using Toxon.Micro.RabbitBlog.Routing.RouteSelection;
+
+namespace Toxon.Micro.RabbitBlog.Local.Host
+{
+    internal class LocalModel : IRoutingModel
+    {
+        private readonly Router<BusRoutingData> _busRouter = CreateRouter<BusRoutingData>();
+        private readonly Router<RpcRoutingData> _rpcRouter = CreateRouter<RpcRoutingData>();
+
+        private static Router<T> CreateRouter<T>() =>
+            new Router<T>(new CompositeRouteSelectionStrategy<T>(
+                new MatchingRoutesSelectionStrategy<T>(),
+                new TopScoringRoutesSelectionStrategy<T>(new RouteScoreComparer()),
+                new RandomRouteSelectionStrategy<T>()
+            ));
+
+        public Task SendAsync(Message message, CancellationToken cancellationToken = default)
+        {
+            var route = _busRouter.Match(message).Single();
+            return route.Data.Handler(message, cancellationToken);
+        }
+
+        public Task<Message> CallAsync(Message message, CancellationToken cancellationToken = default)
+        {
+            var route = _rpcRouter.Match(message).Single();
+            return route.Data.Handler(message, cancellationToken);
+        }
+
+        public Task RegisterHandlerAsync(IRequestMatcher pattern, Func<Message, CancellationToken, Task> handler, RouteExecution execution = RouteExecution.Asynchronous, RouteMode mode = RouteMode.Observe, CancellationToken cancellationToken = default)
+        {
+            _busRouter.Register(string.Empty, pattern, new BusRoutingData(handler));
+            return Task.CompletedTask;
+        }
+
+        public Task RegisterHandlerAsync(IRequestMatcher pattern, Func<Message, CancellationToken, Task<Message>> handler, RouteExecution execution = RouteExecution.Synchronous, RouteMode mode = RouteMode.Capture, CancellationToken cancellationToken = default)
+        {
+            _rpcRouter.Register(string.Empty, pattern, new RpcRoutingData(handler));
+            return Task.CompletedTask;
+        }
+
+        private class BusRoutingData
+        {
+            public Func<Message, CancellationToken, Task> Handler { get; }
+
+            public BusRoutingData(Func<Message, CancellationToken, Task> handler)
+            {
+                Handler = handler;
+            }
+        }
+        private class RpcRoutingData
+        {
+            public Func<Message, CancellationToken, Task<Message>> Handler { get; }
+
+            public RpcRoutingData(Func<Message, CancellationToken, Task<Message>> handler)
+            {
+                Handler = handler;
+            }
+        }
+    }
+}
---
+++ Toxon.Micro.RabbitBlog.Local.Host/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Toxon.Micro.RabbitBlog.Plugins.Reflection;
+
+namespace Toxon.Micro.RabbitBlog.Local.Host
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pluginPaths = DiscoverPluginAssemblies();
+
+            var pluginLoaders = Bootstrapper.LoadPlugins(pluginPaths);
+            var plugins = PluginDiscoverer.Discover(pluginLoaders.Assemblies);
+
+            var model = new LocalModel();
+            foreach (var plugin in plugins)
+            {
+                await Bootstrapper.RegisterPluginAsync(model, plugin);
+            }
+
+            Console.WriteLine($"Running {string.Join(", ", plugins.Select(x => x.ServiceKey))}... press enter to exit!");
+            Console.ReadLine();
+        }
+
+        private static IReadOnlyCollection<string> DiscoverPluginAssemblies()
+        {
+            // navigate to solution root
+            var baseDir = Path.GetFullPath("../../../../", Environment.CurrentDirectory);
+            var pattern = "Toxon.Micro.RabbitBlog.*.dll";
+
+            var files = Directory.GetFiles(baseDir, pattern, SearchOption.AllDirectories);
+            
+            return files
+                .Where(x => x.Contains($"bin{Path.DirectorySeparatorChar}Debug"))
+                .Where(x => !x.EndsWith(".Tests.dll") && !x.EndsWith(".Test.dll"))
+                .OrderByDescending(File.GetLastWriteTimeUtc)
+                .Distinct(new FileNameEqualityComparer())
+                .ToList();
+        }
+    }
+
+    internal class FileNameEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            var xFileName = Path.GetFileName(x)?.ToLower();
+            var yFileName = Path.GetFileName(y)?.ToLower();
+
+            return string.Equals(xFileName, yFileName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return Path.GetFileName(obj)?.ToLower().GetHashCode() ?? 0;
+        }
+    }
+}
