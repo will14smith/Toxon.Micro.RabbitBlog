ac5b67ba5c99d44410f11339514f5a0a6aac7028
Will Smith <will@toxon.co.uk>
2019-05-11 14:22:39 +0100

    Create new function entrypoints

---
+++ Toxon.Micro.RabbitBlog.Serverless.HttpEntry/BaseFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Amazon.Lambda.AspNetCoreServer;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Toxon.Micro.RabbitBlog.Routing;
+using Toxon.Micro.RabbitBlog.Serverless.Core;
+
+namespace Toxon.Micro.RabbitBlog.Serverless.HttpEntry
+{
+    public abstract class BaseFunction : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            AWSSDKHandler.RegisterXRayForAllServices();
+
+            var serviceKey = GetServiceKey();
+            var type = GetServiceType();
+
+            builder
+                .ConfigureServices(services => services.AddSingleton(CreateSender()))
+                .Configure(app =>
+                {
+                    app.UseExceptionHandler("/Error");
+                    app.UseXRay(serviceKey);
+                })
+                .UseStartup(type);
+        }
+        
+        protected abstract string GetServiceKey();
+        protected abstract Type GetServiceType();
+
+        private static IRoutingSender CreateSender()
+        {
+            return new LambdaSender(GetRouterQueueName(), GetRouterFunctionName());
+        }
+
+        public static string GetRouterQueueName()
+        {
+            return Environment.GetEnvironmentVariable("ROUTER_QUEUE_NAME");
+        }
+        public static string GetRouterFunctionName()
+        {
+            return Environment.GetEnvironmentVariable("ROUTER_FUNCTION_NAME");
+        }
+    }
+}
---
+++ Toxon.Micro.RabbitBlog.Serverless.ServiceEntry/BaseFunction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.SQSEvents;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using Newtonsoft.Json;
+using Toxon.Micro.RabbitBlog.Serverless.Core;
+
+namespace Toxon.Micro.RabbitBlog.Serverless.ServiceEntry
+{
+    public abstract class BaseFunction
+    {
+        private readonly LocalModel _handler;
+
+        protected BaseFunction()
+        {
+            AWSSDKHandler.RegisterXRayForAllServices();
+
+            _handler = CreateHandler();
+        }
+
+        protected abstract LocalModel CreateHandler();
+
+        public async Task<MessageModel> HandleDirectAsync(MessageModel requestModel)
+        {
+            var request = requestModel.ToMessage();
+            var response = await _handler.CallAsync(request);
+
+            return new MessageModel(response);
+        }
+
+        public async Task HandleQueueAsync(SQSEvent message)
+        {
+            Console.WriteLine("HandleQueueAsync");
+
+            foreach (var record in message.Records)
+            {
+                var requestModel = JsonConvert.DeserializeObject<MessageModel>(record.Body);
+                var request = requestModel.ToMessage();
+
+                await _handler.SendAsync(request);
+            }
+            Console.WriteLine("HandleQueueAsync done");
+        }
+
+    }
+}
